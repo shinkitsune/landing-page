ec4a4e163d51da8b52885f5bf44a6c01948d7f31
Elton Andrade <eltonandradee@gmail.com>
2021-04-24 14:47:07 -0300

    cloned landing page for course react

---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { GetStaticProps } from 'next'
+
+import SectionHero from 'components/SectionHero'
+import SectionAboutProject from 'components/SectionAboutProject'
+import SectionTech from 'components/SectionTech'
+import SectionConcepts from 'components/SectionConcepts'
+import SectionModules from 'components/SectionModules'
+import SectionAgenda from 'components/SectionAgenda'
+import PricingBox from 'components/PricingBox'
+import SectionAboutUs from 'components/SectionAboutUs'
+import SectionReviews from 'components/SectionReviews'
+import SectionFaq from 'components/SectionFaq'
+import Footer from 'components/Footer'
+import JsonSchema from 'components/JsonSchema'
+
+import client from 'graphql/client'
+import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
+
+const Index = () => (
+  <>
+    <SectionHero />
+    <SectionAboutProject />
+    <SectionTech />
+    <SectionConcepts />
+    <SectionModules />
+    <SectionAgenda />
+    <PricingBox />
+    <SectionAboutUs />
+    <SectionReviews />
+    <SectionFaq />
+    <Footer />
+    <JsonSchema />
+  </>
+)
+
+export const getStaticProp: GetStaticProps = async () => {
+  const { landingPage } = await client.request(GET_LANDING_PAGE)
+
+  console.log(landingPage)
+
+  return {
+    props: {
+      ...landingPage
+    }
+  }
+}
+
+export default Index
